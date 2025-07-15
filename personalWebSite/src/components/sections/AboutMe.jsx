1f44e66b22bdd10187e7d18f2c20ddf1286f4ff4
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-07-16 02:02:42 +0300

    hr update

--- personalWebSite/src/components/sections/AboutMe.jsx
+++ personalWebSite/src/components/sections/AboutMe.jsx
@@ -5,11 +5,14 @@ import EducationExperiance from "./EducationExperiance";
 const AboutMe = () => {
   return (
     <div className="aboutMe-container">
-      <div className="aboutMe-text">
-        Hello, my name is Özay Melih Yıldız. I graduated in Computer Engineering
-        from Süleyman Demirel University. I focus on modern web development,
-        creating responsive user interfaces with React and building scalable
-        backend services with Node.js.
+      <div className="aboutMe-block">
+        <hr />
+        <div className="aboutMe-text">
+          Hello, my name is Özay Melih Yıldız. I graduated in Computer
+          Engineering from Süleyman Demirel University. I focus on modern web
+          development, creating responsive user interfaces with React and
+          building scalable backend services with Node.js.
+        </div>
       </div>
       <EducationExperiance />
     </div>
